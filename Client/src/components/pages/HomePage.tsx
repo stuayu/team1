0f13bd986c099017e65ad95e80bedd8881219d15
B21P006-yuki <B21P006@akita-pu.ac.jp>
2021-06-09 14:27:50 +0900

    表を作りました

--- Client/src/components/pages/HomePage.tsx
+++ Client/src/components/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //import Header from '../templates/Header'
-import Sidebar from '../templates/Sidebar_table';
+import Sidebar from '../templates/Table';
 
 const HomePage: React.FC = () => {
     //return <Header></Header>;
