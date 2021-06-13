055c73c41241ebe8e8c473958c4de671c5d19573
ho-shito <84438318+blue-chart@users.noreply.github.com>
2021-06-13 17:56:18 +0900

    できたと思われる

--- Server/DB/db_subject.py
+++ Server/DB/db_subject.py
@@ -2,18 +2,19 @@ from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from auth_pro import get_current_user, get_current_user_with_refresh_token, create_tokens, authenticate
-from login.auth_pro import get_current_user, get_current_user_with_refresh_token, create_tokens, authenticate, create_user, password_renew
+from login.auth_pro import get_current_user, get_current_user_with_refresh_token, create_tokens, authenticate, create_user
 from DB import db
 
 #ログインした人の履修科目を取り出す
 def get_subject(tokens):
     user1 = get_current_user(tokens)
+    user2 = []
     selectSql = 'Select * from %s' % table
     conn = db.createMysqlConnecter()
     user2 = db.selectData(conn, selectSql)
-    if user1 == user2:
-        subject = user2[0]
-
+    for i in user2:
+        if user1 == user2:
+            subject = user2[i][4,5]
     return subject
 
 
