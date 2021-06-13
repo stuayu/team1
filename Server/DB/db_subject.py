e1d09721131061b1154e2d3b080d56efc636b897
ho-shito <84438318+blue-chart@users.noreply.github.com>
2021-06-13 18:04:21 +0900

    履修教科取得の実装

--- Server/DB/db_subject.py
+++ Server/DB/db_subject.py
@@ -7,14 +7,14 @@ from DB import db
 
 #ログインした人の履修科目を取り出す
 def get_subject(tokens):
-    user1 = get_current_user(tokens)
-    user2 = []
+    user1 = get_current_user(tokens) #ログインしたユーザを取得
+    user2 = [] #DBからデータを収納
     selectSql = 'Select * from %s' % table
     conn = db.createMysqlConnecter()
     user2 = db.selectData(conn, selectSql)
     for i in user2:
-        if user1 == user2:
-            subject = user2[i][4,5]
+        if user1 == user2[i][0]:#ログインしたユーザを探す
+            subject = user2[i][4,5] #履修教科を取得
     return subject
 
 
