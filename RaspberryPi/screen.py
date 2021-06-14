4d45b551b6978d128822109dc379e69140191613
sato-apu <b21p014@akita-pu.ac.jp>
2021-06-15 05:21:41 +0900

    画面の雛形

--- RaspberryPi/screen.py
+++ RaspberryPi/screen.py
@@ -1,20 +1,63 @@
 import tkinter as tk
  
-def btn_click():
+def btn_click1():
 
     #最初のGUIの画面に戻る
     def return_view():
-        baseGround_new_csv.destroy()
+        baseGround_new_csv1.destroy()
 
-    baseGround_new_csv = tk.Tk()
+    baseGround_new_csv1 = tk.Tk()
     # GUIの画面サイズ
-    baseGround_new_csv.geometry('300x200')
+    baseGround_new_csv1.geometry('300x200')
     #GUIの画面タイトル
-    baseGround_new_csv.title('結果画面')
+    baseGround_new_csv1.title('結果画面1')
+    #ラベル
+    label = tk.Label(baseGround_new_csv1, text = '時間内での出席を確認しました', foreground='white', background='black')
+    label.pack()
     # ボタン
-    btn_return = tk.Button(baseGround_new_csv, text='待機画面に戻る', command=return_view)
+    btn_return = tk.Button(baseGround_new_csv1, text='待機画面に戻る', command=return_view)
     btn_return.pack()
-    baseGround_new_csv.mainloop()
+    baseGround_new_csv1.mainloop()
+
+def btn_click2():
+
+    #最初のGUIの画面に戻る
+    def return_view():
+        baseGround_new_csv2.destroy()
+
+    baseGround_new_csv2 = tk.Tk()
+    # GUIの画面サイズ
+    baseGround_new_csv2.geometry('300x200')
+    #GUIの画面タイトル
+    baseGround_new_csv2.title('結果画面2')
+    #ラベル
+    label = tk.Label(baseGround_new_csv2, text = '遅刻での出席を確認しました', foreground='white', background='black')
+    label.pack()
+    # ボタン
+    btn_return = tk.Button(baseGround_new_csv2, text='待機画面に戻る', command=return_view)
+    btn_return.pack()
+    baseGround_new_csv2.mainloop()
+
+def btn_click3():
+
+    #最初のGUIの画面に戻る
+    def return_view():
+        baseGround_new_csv3.destroy()
+
+    baseGround_new_csv3 = tk.Tk()
+    # GUIの画面サイズ
+    baseGround_new_csv3.geometry('300x200')
+    #GUIの画面タイトル
+    baseGround_new_csv3.title('結果画面3')
+    #ラベル
+    label = tk.Label(baseGround_new_csv3, text = 'この教科の名簿に登録されていません', foreground='white', background='red')
+    label.pack()
+    label = tk.Label(baseGround_new_csv3, text = 'もう一度部屋を確認してください', foreground='white', background='red')
+    label.pack()
+    # ボタン
+    btn_return = tk.Button(baseGround_new_csv3, text='待機画面に戻る', command=return_view)
+    btn_return.pack()
+    baseGround_new_csv3.mainloop()
 
 baseGround = tk.Tk()
 # GUIの画面サイズ
@@ -24,12 +67,17 @@ baseGround.title('待機画面')
 
 #ラベル
 label = tk.Label(text = 'カードをタップしてください', foreground='white', background='black')
-label.place(x=30, y=70)
+label.pack()
 
 # ボタン
-btn = tk.Button(baseGround, text='次の画面に移動', command=btn_click)
-btn.place(x=120, y=140)
+btn1 = tk.Button(baseGround, text='時間内に出席', command=btn_click1)
+btn1.pack()
 
+btn2 = tk.Button(baseGround, text='遅刻', command=btn_click2)
+btn2.pack()
+
+btn3 = tk.Button(baseGround, text='教科間違い', command=btn_click3)
+btn3.pack()
  
 #表示
 baseGround.mainloop()
