1824f7079c051da1c43c81b01560a618864087a4
stuayu <60843145+stuayu@users.noreply.github.com>
2021-06-15 11:22:27 +0900

    コメントの追加

--- Server/main.py
+++ Server/main.py
@@ -2,9 +2,9 @@
 from fastapi import FastAPI, Depends, Form
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
-from DB import db  # DBフォルダにいるdb.pyの関数を読み込み
+# DBフォルダにいるdb.pyの関数を読み込み．なお同じフォルダ内のファイルをインポートする場合は","で追加
+from DB import db, db_subject
 from routers import login
-from DB import db_subject
 
 # uvicorn main:app --reload --host 0.0.0.0
 app = FastAPI()
@@ -25,10 +25,14 @@ class TestParam(BaseModel):
     param2: str
 
 # curl http://localhost:8000/
+# メモ
+# tags=[""]で指定した文字列がグループ名になります．
+# def を書いた下に"""文字列"""このように書くとブラウザにコメントを表示できます．
+# Form(...)を指定した場合の動作：URLに書かれた文字列が残らないよって，積極的に利用してほしい．
 
 
 @app.post("/user/check/", tags=["RaspberryPi"])
-async def check_idm(teacher: str = Form(...), IDm: str = Form(...)):
+async def check_idm(teacher_id: str = Form(...), student_idm: str = Form(...)):
     """idmと教職員データを利用して
        戻り値に出席，遅刻を返す 
     """
@@ -54,8 +58,7 @@ def get_table(table: str):  # table変数を文字列に定義
     conn = db.createMysqlConnecter()    # データベースにログイン
     return db.selectData(conn, selectSql)    # データベースから情報取得
 
-#ユーザの教科を取得(仮)
-#@app.get("/db/",tags=["Subject"])
-#def get_db_subject(tokens):
+# ユーザの教科を取得(仮)
+# @app.get("/db/",tags=["Subject"])
+# def get_db_subject(tokens):
  #   return get_subject(tokens)
-
