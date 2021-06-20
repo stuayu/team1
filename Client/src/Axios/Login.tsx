10eafa859f006418cb5ceebae402ac6f169c08b9
B21P006-yuki <B21P006@akita-pu.ac.jp>
2021-06-20 23:07:00 +0900

    post送信したかった

---
+++ Client/src/Axios/Login.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState()
+
+  const handleSubmit = async e => {
+    
+  };
+
+// if there's a user show the message below
+  if (user) {
+    return <div>{user.name} is loggged in</div>;
+  }
+
+  // if there's no user, show the login form
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        value={username}
+        placeholder="enter a username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <div>
+        <label htmlFor="password">password: </label>
+        <input
+          type="password"
+          value={password}
+          placeholder="enter a password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default App;
