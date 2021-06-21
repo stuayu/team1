0c6b7658837fa84cda6fc83965ffe41eaab9d518
stuayu <60843145+stuayu@users.noreply.github.com>
2021-06-22 07:58:24 +0900

    ラズパイからのデータ受け取り部分の実装
    実装していますが，なかなか難しいですね．
    もう少し時間がほしいです．

--- Server/DB/check_idm.py
+++ Server/DB/check_idm.py
@@ -1,17 +1,42 @@
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
 from peewee import *
+import datetime
 from DB.db_access import db_login
+from key.readkey import get_key  # AESキーの取得
+
 
 db = db_login()
 
 
-def get_data(teacher_id: str, student_idm: str):
-    """ここにメイン関数を作る"""
+def get_data(teacher_id: str, student_idm: str, iv: bytes):
+    """ここにメイン関数を作る
+    teacher_id : AES公開キーで暗号化
+    student_idm: AES公開キーで暗号化
+    """
+    cipher = AES.new(key=get_key, mode=AES.MODE_CBC, iv=iv)    # AESの秘密キーで復号化準備
+    teacher = cipher.decrypt(teacher_id) # AESの秘密キーで復号化
+    student = cipher.decrypt(student_idm)  # AESの秘密キーで復号化
+    plain_teacher = Padding.unpad(teacher, 32, 'pkcs7')
+    plain_student = Padding.unpad(student, 32, 'pkcs7')
+
+
+    # debug
+    print('teacherの復号結果:' + plain_teacher)
+    print('studentの復号結果:' + plain_student)
+
+    #subject = get_subject(teacher)
+
+    #result = create_attend(subject, student)
+    return 0
 
 
 def get_subject(teacher_id: str):
     """ここに現在の日時と曜日と教員のIDから出欠登録しようとしている科目の決定
        戻り値：科目ID
     """
+
+    return subject
 
 
 def create_attend(subject_id: str, student_idm: str):
