eb73361cd198bac84a5ef0ce962052a492cefd1c
stuayu <60843145+stuayu@users.noreply.github.com>
2021-06-24 17:42:35 +0900

    一時保存

--- Server/DB/check_idm.py
+++ Server/DB/check_idm.py
@@ -1,15 +1,17 @@
 import datetime
+from fastapi import HTTPException
 from DB import db
-from key.readkey import get_key  # AESキーの取得
 
 
-def get_data(teacher_id: str, student_idm: str, iv: bytes):
+def get_data(teacher_id: str, student_idm: str):
     """ここにメイン関数を作る
     """
+    subject_id = get_subject(teacher_id)
 
-    #subject = get_subject(teacher)
+    if subject_id == 0:
+        raise HTTPException(status_code=401, detail='すでに登録時刻を過ぎているか，開始していません．')
 
-    #result = create_attend(subject, student)
+    result = create_attend(subject_id, student_idm)
     return 0
 
 
@@ -18,20 +20,29 @@ def get_subject(teacher_id: str):
        戻り値：講義ID
     """
     temp = []  # 一時的に配列に代入するよう
-    date = datetime.date.today()  # 今日の日付を取得 (2021-06-23)
-    date = "{}/{}/{}".format(date.year, date.month,
-                             date.day)  # 日付の形式を変更(2021/6/23)
+    dt = datetime.datetime.now()  # 今日の日付を取得 (2021-06-23)
+    date = "{}/{}/{}".format(dt.year, dt.month,
+                             dt.day)  # 日付の形式を変更(2021/6/23)
+    time= "{}:{}".format(dt.hour,dt.minute) #取得した時間 (16:41)
     # time-rule テーブルを操作
     # %sを変数 date に置き換える
-    selectSql = "Select `日付`, `時間割`,`回数` FROM `subject-rules2` WHERE  `日付`=%s" % date
+    selectSql_1 = "Select `日付`, `時間割`,`回数` FROM `subject-rules2` WHERE  `日付`='%s'" % date
     conn = db.createMysqlConnecter()
 
-    temp = db.selectData(conn, selectSql)
+    temp = db.selectData(conn, selectSql_1)
 
-    time_rule = temp[1][2]  # 時間割データ
-    number = temp[1][3]  # 第何回の講義か
-    
+    time_rule: str = temp[0][1]  # 時間割データ
+    #number: int = temp[0][2]  # 第何回の講義か
 
+    #受付開始時間以上遅刻限度以下かつ時間割と教員IDがあっている場合のみ実行される．
+    selectSql_2 = "Select `講義ID`,`時間割`,`教員ID`,`受付開始`,`遅刻限度` FROM `time-rules` WHERE `時間割`='%s'&& `教員ID`='%s' && '%s' BETWEEN `受付開始` AND `遅刻限度`" % (time_rule , teacher_id , time)
+
+    temp = db.selectData(conn, selectSql_2)
+
+    if len(temp) == 0:
+        return 0
+
+    subject:str = temp[0][0]
 
     return subject
 
@@ -42,3 +53,7 @@ def create_attend(subject_id: str, student_idm: str):
        一致するデータがなければエラーを返す．
        これを元にメインではラズパイに返すメッセージ文を生成し返す．
     """
+
+if __name__ == '__main__':
+    res = get_subject('P003')
+    print(res)
