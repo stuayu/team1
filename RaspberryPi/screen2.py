8b4a9c8deb917386623396e08ebef442c5c13dcd
ho-shito <84438318+blue-chart@users.noreply.github.com>
2021-06-27 14:15:09 +0900

    デザイン変更

--- RaspberryPi/screen2.py
+++ RaspberryPi/screen2.py
@@ -23,24 +23,28 @@ if __name__ == "__main__":
 
 
     # メインフレームの作成と設置
-    frame = tk.Frame(root)
+    frame = tk.Frame(root,bg='RoyalBlue2')
     frame.grid(row=0, column=0, sticky="nsew", pady=20)
 
     # 各種ウィジェットの作成
-    label1_frame = ttk.Label(frame, text = '教員IDと講義IDを入力してください',foreground='RoyalBlue2',font=("20"))
-    label2_frame = ttk.Label(frame, text='教員ID')
+    label1_frame = ttk.Label(frame, text = '教員IDと講義IDを入力してください',foreground='White',font=("20"),background='RoyalBlue2')
+    label2_frame = ttk.Label(frame, text='教員ID',foreground='White',background='RoyalBlue2')
     entry1_frame = ttk.Entry(frame,width=30)
-    label3_frame = ttk.Label(frame,text='講義ID')
+    label3_frame = ttk.Label(frame,text='講義ID',foreground='White',background='RoyalBlue2')
     entry2_frame = ttk.Entry(frame,width=30)
     button_change = ttk.Button(frame, text="OK", command=change_app)
 
-    # 各種ウィジェットの設置
-    label1_frame.pack()
-    label2_frame.pack()
-    entry1_frame.pack()
-    label3_frame.pack()
-    entry2_frame.pack()
-    button_change.pack()
+    # 各種ウィジェットの設置(ピクセル単位で指定)
+    label1_frame.place(x=240,y=0)
+    label2_frame.place(x=365,y=40)
+    entry1_frame.place(x=300,y=70)
+    label3_frame.place(x=365,y=100)
+    entry2_frame.place(x=300,y=130)   
+    button_change.place(x=350,y=250)
+
+
+
+
 
     #フレーム作成
     frame2 = ttk.Frame(root)
