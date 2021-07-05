21b7fabbcf99e3a7918505f27ec28e82819b8301
ho-shito <84438318+blue-chart@users.noreply.github.com>
2021-07-05 23:22:48 +0900

    未完成

--- Server/DB/db_subject.py
+++ Server/DB/db_subject.py
@@ -20,7 +20,7 @@ def get_subject_teacher(token):
             subject.append(user2[i][4]) #履修教科を取得
             if user2[i][5] != None: #2つめの科目がある場合追加する
                 subject.append(user2[i][5])
-
+            
     return subject
 
 #ログインした人(学生)の履修教科を取り出す関数
@@ -28,19 +28,28 @@ def get_subject_student(token):
     user1 = get_current_user_from_token(token,'access_token') #ログインしたユーザを取得
     user2 = [] #DBからデータを収納
     subject =[] #担当教科
-    selectSql = "Select * from team1" #team1から学生のデータを取り出す
+    selectSql = "Select * from student_all" #student_allから学生のデータを取り出す
     conn = db.createMysqlConnecter()
     user2 = db.selectData(conn, selectSql)
+
+    selectSql = "Select * from subject_rules" #subject_rulesからデータを取り出す。
+    subject_data = db.selectData(conn, selectSql)
 
     for i in range(len(user2)): #user2の長さの分だけ繰り返す
         if user1.name == user2[i][1]:#ログインした人の名前とDBから取得したデータからユーザを探す
             subject = user2[i][5:] #履修教科を取得
             break
-    t = [] # 空白を取り除いた教科を代入するためのタプル
+    t = [] # 空白を取り除いた教科(イニシャル)を代入するためのタプル
     for i in range(len(subject)):
         if len(subject[i]) > 0: #空白以外
             t.append(subject[i]) #空白以外を代入
-    return t
+
+    sub = [] #教科名を代入するためのタプル
+    for i in range(len(t)):
+        if subject_data[i][0] == t[i]:
+            sub.append(subject_data[i][1])
+
+    return sub
 
 
 
