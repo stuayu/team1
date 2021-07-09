a091083d40ad3fd104539e1509094e669bec2f0a
stuayu <60843145+stuayu@users.noreply.github.com>
2021-07-09 14:27:13 +0900

    mainから一部の関数ををrouterに移動

--- Server/main.py
+++ Server/main.py
@@ -5,14 +5,14 @@ from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 # DBフォルダにいるdb.pyの関数を読み込み．なお同じフォルダ内のファイルをインポートする場合は","で追加
 from DB import db, db_subject
-from routers import login
-from DB.db_subject import get_subject_teacher, get_subject_student
+from routers import login, front
 from DB.check_idm import get_data
 
 # uvicorn main:app --reload
 app = FastAPI()
 # routers/login からインポートする
 app.include_router(login.router)
+app.include_router(front.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,9 +40,8 @@ async def check_idm(teacher_id: str = Form(...), student_idm: str = Form(...)):
        戻り値に出席，遅刻を返す 
     """
     get_data(teacher_id, student_idm)
-    
-    return {"message": "登録が完了しました．"}
 
+    return {"message": "登録が完了しました．"}
 
 
 @app.get("/")
@@ -58,23 +57,9 @@ def post_root(testParam: TestParam):
     return testParam
 
 
-@app.get("/db/{table}",tags=["DB"])     # docsに表示されるURL
+@app.get("/db/{table}", tags=["DB"])     # docsに表示されるURL
 def get_table(table: str):  # table変数を文字列に定義
     """データベースにあるテーブル名を入力すると中身がそのまま帰ってくる機能です(危険)"""
     selectSql = 'Select * from %s' % table  # %sを変数 table に置き換える
     conn = db.createMysqlConnecter()    # データベースにログイン
     return db.selectData(conn, selectSql)  # データベースから情報取得
-
-#ユーザ(教員)の教科を取得
-@app.post("/db/",tags=["Subject"])
-def get_db_subject(token: str=Form(...)):
-    """アクセストークンを利用して先生の担当科目を返す"""
-    return get_subject_teacher(token)
-    
-#ユーザ(学生)の教科を取得
-@app.post("/db2/",tags=["Subject"])
-def get_db_subject(token: str = Form(...)):
-    """アクセストークンを利用して学生の担当科目を返す"""
-    return get_subject_student(token)
-
-
---
+++ Server/routers/front.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, Form
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from DB.db_subject import get_subject_teacher, get_subject_student
+from DB.db_front import get_number, print_attend
+
+router = APIRouter()
+
+
+# ユーザ(教員)の教科を取得
+@app.post("/db/", tags=["Subject"])
+def get_db_subject(token: str = Form(...)):
+    """アクセストークンを利用して先生の担当科目を返す"""
+    return get_subject_teacher(token)
+
+# ユーザ(学生)の教科を取得
+@app.post("/db2/", tags=["Subject"])
+def get_db_subject(token: str = Form(...)):
+    """アクセストークンを利用して学生の担当科目を返す"""
+    return get_subject_student(token)
+
+# 講義回数をJSONで返す
+@router.post("/number", tags=["Subject"])
+def get_number1(subject_id: str = Form(...)):
+    """教科IDを送信されたら科目IDと実施回数をJSONで返す"""
+    return get_number(subject_id)
+
+# 出欠状況検索
+@router.post("/attend", tags=["Subject"])
+def print_db_attend(subject_id: str = Form(...), number: int = Form(...), userid: str = Form(...)):
+    """出欠状況の検索"""
+    return print_attend(subject_id, number, userid)
