ee30020cd113b8bc88d86e4bbd72b0dc783d26de
ho-shito <84438318+blue-chart@users.noreply.github.com>
2021-07-11 21:27:34 +0900

    まだできていないです

--- Server/DB/db_csv.py
+++ Server/DB/db_csv.py
@@ -1,17 +1,12 @@
-from DB import db
 import csv
-
-#file = open('student_attendance.csv','w')
-#w = csv.writer(file)
+import db
 
 selectSql = "Select * from student_all"
 conn = db.createMysqlConnecter()
-c = conn.cursor()
+data = db.selectdata(conn,selectSql)
 
-with open('./Server/student_attendance.csv',"w",encoding="utf-8") as f:
-    for row in c.execute('SELECT * FROM student_attendance ORDER BY No DESC'):
-        f.write(','.join([str(c) for c in row]) + '\n')
-
-conn.close()
-
+with open('student_attendance.csv','a') as f:
+    w = csv.writer(f)
+    for i in range(len(data)):
+        w.writerow(data[i])
 
