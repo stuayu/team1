7c1276b0d649b9b2b5818e91dc562f96301c52f2
stuayu <60843145+stuayu@users.noreply.github.com>
2021-07-17 10:51:58 +0900

    動作する状態

--- Client/src/components/templates/Axios-api2.tsx
+++ Client/src/components/templates/Axios-api2.tsx
@@ -8,43 +8,55 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
 
-let res
-  postdata()
-  async function postdata() {
-    var param = new URLSearchParams();
-    const token = localStorage.getItem('token')?.toString()
-    if (token != null) {
-      param.append('token', token)
-    }
-    try {
-      res = await axios.post('http://localhost:8000/getSub/', param)
-      console.log(res)
-      await createTable()
-    } catch(err){
-      res = err.response
-    }
-  }
-  interface Data {
-    id: string;
-    sub_name: string;
-  }
+const DATA = 'http://localhost:8000/getSub/'
+// トークンをローカルストレージから取得する
+var param = new URLSearchParams();
+const token = localStorage.getItem('token')?.toString()
+if (token != null) {
+  param.append('token', token)
+}
+interface Data {
+  id: string;
+  sub_name: string;
+}
 
-  function createData(
-    id: string,
-    sub_name: string,
-  ): Data {
-    return { id, sub_name };
-  }
+function createData(
+  id: string,
+  sub_name: string,
+): Data {
+  return { id, sub_name };
+}
 
-  const rows: Data[] = [];
-
-  function createTable() {
-    for (let i = 0; i < res.data.id.length; i += 1) {
-    rows.push(createData( res.data.id[i], res.data.sub_name[i]));
-    }
-  }
+const rows: Data[] = [];
 
 export default function BasicTable() {
+  
+  const [Id, setId] = React.useState([]);
+  const [Sub_name, setSub_name] = React.useState([]);
+
+  // 参考ページ(https://qiita.com/Kouichi_Itagaki/items/c8e05f084fe88a086100)
+  React.useEffect(() => {
+    postdata()
+  }, [])
+
+  //非同期でサーバーからデータを取得
+  const postdata = async () => {
+    try {
+      const res = await axios.post(DATA, param);
+      setId(res.data.id)
+      setSub_name(res.data.sub_name)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function CreateTable() {
+    rows.length = 0; //配列内をリセット(変な値が残ることを阻止する)
+    for (let i = 0; i < Id.length; i += 1) {
+    rows.push(createData(Id[i], Sub_name[i]));
+    }
+  }
+  CreateTable();
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
